3aa7fc06a3bfe3d87ed17fc2a1221c38c91de67c
jhgyun <jhgyun@users.noreply.github.com>
2016-07-27 11:58:44 +0900

    Update ObjectLoader.ts

--- Three.ts/Three.ts/loaders/ObjectLoader.ts
+++ Three.ts/Three.ts/loaders/ObjectLoader.ts
@@ -12,7 +12,6 @@ namespace THREE
 
         constructor(manager?: LoadingManager)
         {
-
             this.manager = (manager !== undefined) ? manager : THREE.DefaultLoadingManager;
             this.texturePath = '';
         };
